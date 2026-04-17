08cdcf44fa4720f8649c07889178b0dfda63d54c
danielcb-glitch <eld4nic@gmail.com>
2026-04-17 16:25:09 -0500

    Update onpe.js

--- api/onpe.js
+++ api/onpe.js
@@ -10,12 +10,27 @@ export default async function handler(req, res) {
       url = "https://resultadoelectoral.onpe.gob.pe/presentacion-backend/resumen-general/participantes?idEleccion=10&tipoFiltro=eleccion";
     }
 
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      headers: {
+        "User-Agent": "Mozilla/5.0",
+        "Accept": "application/json"
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error("Error en API ONPE");
+    }
+
     const data = await response.json();
 
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.status(200).json(data);
+
   } catch (error) {
-    res.status(500).json({ error: "Error en proxy" });
+    console.error("ERROR PROXY:", error);
+    res.status(500).json({
+      error: "Error en proxy",
+      detalle: error.message
+    });
   }
 }
